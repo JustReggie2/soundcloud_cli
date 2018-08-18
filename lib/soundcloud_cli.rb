33b361bb113be56cbba0a6ac1a3fe64101cb617e
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-18 22:26:37 +0000

    Added Nokogiri gem.

--- lib/soundcloud_cli.rb
+++ lib/soundcloud_cli.rb
@@ -1,3 +1,6 @@
 #cli controller
 require_relative "soundcloud_cli/version"
 require_relative "soundcloud_cli/cli"
+require_relative "soundcloud_cli/cli"
+require 'nokogiri'
+require 'open-uri'
---
+++ lib/soundcloud_cli/track.rb
@@ -0,0 +1,10 @@
+class Track
+  attr_accessor :name, :artist, :likes, :plays, :url
+
+  @@all = []
+
+  
+
+
+
+end
