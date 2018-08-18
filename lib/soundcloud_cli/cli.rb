9dd0d9e4383c23741322b3a27ba160a9dccd5f0b
Reginald T Carter iI <reginaldcarterii@gmail.com>
2018-08-18 21:23:20 +0000

    Added base method for list_tracks & play_track.

--- lib/soundcloud_cli/cli.rb
+++ lib/soundcloud_cli/cli.rb
@@ -2,7 +2,32 @@
 class ScCli
 
   def call
-    puts "Here is your track list:"
+    list_tracks
   end
 
+  def list_tracks
+    u_i = nil
+    puts "Would you like tracks listed by 'likes' or 'plays'?"
+    while u_i != "exit"
+      u_i = gets.strip.downcase
+      case u_i
+      when "likes"
+        puts "Tracks listed by likes."
+        play_track
+      when "plays"
+        puts "Tracks listed by plays"
+        play_track
+      when "exit"
+        puts "Thank you for using gem!"
+      else
+        puts "invalid entry"
+      end
+    end
+  end
+
+  def play_track
+    # after list_tracks...
+    puts "Please enter number of track you'd like to hear?"
+
+  end
 end
